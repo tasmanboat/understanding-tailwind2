581c36c8dce8913f87f08f486179def85ee33aa1
noname <you@example.com>
2022-05-10 16:09:19 +0800

    update HttpClientInMemoryWebApiModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ The HttpClientInMemoryWebApiModule module intercepts HTTP requests
 and returns simulated server responses.
 Remove it when a real server is ready to receive requests.
 */
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false, passThruUnknownUrl: true, delay: 500 }),
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false, passThruUnknownUrl: true, delay: 0 }),
 // #endregion
 
     AppRoutingModule,
