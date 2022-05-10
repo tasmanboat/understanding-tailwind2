b4ec329eb669d8a662f08064286d5605560e4630
noname <you@example.com>
2022-05-10 22:09:31 +0800

    update markdown.pipe.ts

--- src/app/shared/pipes/markdown.pipe.ts
+++ src/app/shared/pipes/markdown.pipe.ts
@@ -11,7 +11,7 @@ export class MarkdownPipe implements PipeTransform {
     const renderer = new marked.Renderer();
     const linkRenderer = renderer.link;
     renderer.link = (href, title, text) => {
-      const localLink = href!.startsWith(`${location.protocol}//${location.hostname}`);
+      const localLink = href?.startsWith(`${location.protocol}//${location.hostname}`);
       const html = linkRenderer.call(renderer, href, title, text);
       return localLink ? html : html.replace(/^<a /, `<a target="_blank" rel="noreferrer noopener nofollow" `);
     };
